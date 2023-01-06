d66e4751a60cc8f7fc9920dc295d3df81039246e
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-06 14:29:36 +0530

    [Bindhu]Add: Retreive Top Records Of Age

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Buffers;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { SSN = 1, Name = "Bindhu", Address = "Bangalore", Age = 30 });
+            people.Add(new Person() { SSN = 2, Name = "Sunil", Address = "Bangalore", Age = 75});
+            people.Add(new Person() { SSN = 3, Name = "Rekha", Address = "Bangalore", Age = 40 });
+            people.Add(new Person() { SSN = 4, Name = "Parnika", Address = "Bangalore", Age = 3});
+            people.Add(new Person() { SSN = 5, Name = "Meena", Address = "Bangalore", Age = 19});
+            foreach (Person person in people)
+            {
+                Console.WriteLine("SSN={0}\tName={1}\tAddress={2}\tAge={3})", person.SSN, person.Name, person.Address, person.Age);
+            }
+
+            Operations operations = new Operations();
+           operations.RetreiveTopRecordsOfAge(people);
+        }
+    }
+}
